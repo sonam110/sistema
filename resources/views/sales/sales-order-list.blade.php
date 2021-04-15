e49a75c3c1b6aee6c6aa704cb0dd840d54df9585
Jagaib <jagaiberlinsky@gmail.com>
2021-04-15 03:51:51 -0300

    correct payments made

--- resources/views/sales/sales-order-list.blade.php
+++ resources/views/sales/sales-order-list.blade.php
@@ -708,7 +708,8 @@
 	                                {{ Form::select('cmbaction', array(
 	                                ''              => '-- Estado del envío --',
 	                                'Process'       => 'Pendiente',
-	                                'Delivered'     => 'Entregado'),
+	                                'Cancel'      	=> 'Cancelado',
+																	'Delivered'     => 'Entregado'),
 	                                '', array('class'=>'form-control','id'=>'cmbaction'))}}
 	                            </div>
 	                        </div>
